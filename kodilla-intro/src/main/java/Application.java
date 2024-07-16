53a5ce26ab2e0faf053f9d7b3f7ad8e6f8bbe0b6
Kinga Kisiel <kingajulitakisiel@gmail.com>
2024-07-16 17:20:17 +0200

    fix 2.6

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,9 +1,13 @@
+import com.kodilla.ApplicationUser;
+
 public class Application {
     public static void main(String[] args) {
         String name = "Adam";
         double age = 40.5;
-        double height = 178;
+        int height = 178;
 
+        ApplicationUser applicationUser = new ApplicationUser(name, age, height);
+        System.out.println(applicationUser.check());
 
     }
 }
--- kodilla-intro/src/main/java/com/kodilla/ApplicationUser.java
+++ kodilla-intro/src/main/java/com/kodilla/ApplicationUser.java
@@ -1,23 +1,25 @@
 package com.kodilla;
 
 public class ApplicationUser {
-   private String name;
-   private int age;
-   private int height;
+    private String name;
+    private double age;
+    private int height;
 
-    public ApplicationUser(String name, int age, int height) {
+    public ApplicationUser(String name, double age, int height) {
         this.name = name;
         this.age = age;
         this.height = height;
     }
-    public void check(){
 
-        if (name != null) {
-            if (age > 30 && height > 160) {
+    public Object check() {
+
+        if (this.name != null) {
+            if (this.age > 30 && this.height > 160) {
                 System.out.println("User is older than 30 and taller than 160cm");
             } else {
                 System.out.println("User is 30 (or younger) or 160cm (or shorter)");
             }
         }
+        return null;
     }
 }
