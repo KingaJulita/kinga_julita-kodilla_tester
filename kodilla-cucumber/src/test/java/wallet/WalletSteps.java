1dcbc962a9e61d8032dc14e51600914485cb09ef
Kinga Kisiel <kingajulitakisiel@gmail.com>
2024-09-30 19:34:50 +0200

    14.4

--- kodilla-cucumber/src/test/java/wallet/WalletSteps.java
+++ kodilla-cucumber/src/test/java/wallet/WalletSteps.java
@@ -38,6 +38,12 @@ public class WalletSteps {
     public void $200_should_be_dispensed() {
         assertEquals(200, cashSlot.getContents());
     }
+
+    @Then("the balance of my wallet should be ${int}")
+    public void the_balance_of_my_wallet_should_be_$(Integer expectedBalance) {
+        assertEquals(expectedBalance, wallet.getBalance(), "Incorrect wallet balance");
+    }
+
     @When("I request $300")
     public void i_request_$300() {
         Cashier cashier = new Cashier(cashSlot);
@@ -51,7 +57,35 @@ public class WalletSteps {
 
     @Then("I should be told that I have insufficient funds")
     public void i_should_be_told_that_I_have_insufficient_funds() {
-        // This is validated by the exception thrown in the "When" step
     }
 
+    @Given("there is ${int} in my wallet")
+    public void there_is_$100_in_my_wallet(Integer amount) {
+        wallet.deposit(amount);
+        assertEquals(amount, wallet.getBalance(), "Incorrect wallet balance after deposit");
+    }
+
+    @When("I withdraw ${int}")
+    public void i_withdraw_$200(Integer amount) {
+        Cashier cashier = new Cashier(cashSlot);
+        assertThrows(IllegalArgumentException.class, () -> cashier.withdraw(wallet, amount), "Insufficient funds");
+    }
+
+    @When("I check the balance of my wallet")
+    public void i_check_the_balance_of_my_wallet() {
+    }
+
+    @Then("nothing should be dispensed")
+    public void nothing_should_be_dispensed() {
+        assertEquals(0, cashSlot.getContents(), "Money was dispensed despite insufficient funds");
+    }
+
+    @Then("I should be told that I don't have enough money in my wallet")
+    public void i_should_be_told_that_I_dont_have_enough_money_in_my_wallet() {
+    }
+
+    @Then("I should see that the balance is ${int}")
+    public void i_should_see_that_the_balance_is_$100(Integer expectedBalance) {
+        assertEquals(expectedBalance, wallet.getBalance(), "Incorrect wallet balance displayed");
+    }
 }
--- kodilla-cucumber/src/test/resources/wallet/cash_withdrawal.feature
+++ kodilla-cucumber/src/test/resources/wallet/cash_withdrawal.feature
@@ -4,11 +4,13 @@ Feature: Cash Withdrawal
     Given I have deposited $200 in my wallet
     When I request $30
     Then $30 should be dispensed
+    And the balance of my wallet should be $170
 
   Scenario: Full withdrawal from a wallet
     Given I have deposited $200 in my wallet
     When I request $200
     Then $200 should be dispensed
+    And the balance of my wallet should be $0
 
   Scenario: Attempting to withdraw more than available balance
     Given I have deposited $200 in my wallet
